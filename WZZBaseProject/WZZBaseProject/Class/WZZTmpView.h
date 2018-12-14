666f8004c3cd6b1c17b00e6c4103b6ebc54a7035
mac <mac@macdeMacBook-Pro.local>
2018-12-14 13:44:52 +0800

    加入WZZSuperXibView

---
+++ WZZBaseProject/WZZBaseProject/Class/WZZTmpView.h
@@ -0,0 +1,17 @@
+//
+//  WZZTmpView.h
+//  WZZBaseProject
+//
+//  Created by mac on 2018/12/14.
+//  Copyright © 2018 王泽众. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface WZZTmpView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ WZZBaseProject/WZZBaseProject/Class/WZZTmpView.m
@@ -0,0 +1,21 @@
+//
+//  WZZTmpView.m
+//  WZZBaseProject
+//
+//  Created by mac on 2018/12/14.
+//  Copyright © 2018 王泽众. All rights reserved.
+//
+
+#import "WZZTmpView.h"
+
+@implementation WZZTmpView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
